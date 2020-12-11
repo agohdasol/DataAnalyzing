06cf731a72965bef00794132802f37c83c4dc558
DESKTOP-I49D4M0\agohd <agohdasol@gmail.com>
2020-12-12 00:29:45 +0900

    .net core에서는 워크시트 0인덱스가 기본값인것 반영

--- ExcelReader/Program.cs
+++ ExcelReader/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Data;
 using OfficeOpenXml;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ExcelReader
 {
@@ -34,43 +35,35 @@ namespace ExcelReader
                 //return null;
             }
 
-            int workSheetsCounter = excelPackage.Workbook.Worksheets.Count; //워크시트 인덱스와 데이터테이블 인덱스가 다른경우?
+            int workSheetsCounter = excelPackage.Workbook.Worksheets.Count;
             DataTable[] dataTable = new DataTable[workSheetsCounter];
             ExcelWorksheet[] worksheets = new ExcelWorksheet[workSheetsCounter];
 
-            bool is0BaseWorkSheets = true;  //인덱스가 0부터 시작하는 워크시트인지 여부
             for (int i = 0; i < workSheetsCounter; i++)
             {
-                try
-                {
-                    worksheets[i] = excelPackage.Workbook.Worksheets[i];
-                }
-                catch (IndexOutOfRangeException)
-                {
-                    workSheetsCounter += 1; //워크시트 인덱스가 1부터 시작하는 경우에는 인덱스 0인 경우 패스하고 시트 갯수 값을 1 증가시킴
-                    is0BaseWorkSheets = false;
-                    continue;
-                }
+                worksheets[i] = excelPackage.Workbook.Worksheets[i];
+                dataTable[i] = new DataTable();
             }
-            //데이터테이블 초기화할것
+
             //check if the worksheet is completely empty
-            if ((is0BaseWorkSheets? worksheets[0].Dimension : worksheets[1].Dimension) == null)
+            if (worksheets[0].Dimension == null)
             {
                 return dataTable;
             }
-
-            //create a list to hold the column names
-            List<string> columnNames = new List<string>();
-
-            //needed to keep track of empty column headers
-            int currentColumn = 1;
-
-            for(int i = (is0BaseWorkSheets? 0:1); i < workSheetsCounter; i++)
+            
+            for (int i = 0; i < workSheetsCounter; i++)
             {
+                //create a list to hold the column names
+                List<string> columnNames = new List<string>();
+
+                //needed to keep track of empty column headers
+                int currentColumn = 1;
                 //loop all columns in the sheet and add them to the datatable
-                foreach (var cell in worksheets[i].Cells[1, 1, 1, worksheets[i].Dimension.End.Column])
+                foreach (var cell in worksheets[i].Cells[1, 1, 1, worksheets[i].Dimension.End.Column])  //칼럼명이 없는경우, foreach문이 돌지 않음
                 {
-                    string columnName = cell.Text.Trim();
+                    string columnName = cell.Text.Trim();   
+                    if (columnName == null || columnName == "")
+                        columnName = "Empty";
 
                     //check if the previous header was empty and add it if it was
                     if (cell.Start.Column != currentColumn)
@@ -85,11 +78,11 @@ namespace ExcelReader
 
                     //count the duplicate column names and make them unique to avoid the exception
                     //A column named 'Name' already belongs to this DataTable
-                    //int occurrences = columnNames.Count(x => x.Equals(columnName));
-                    //if (occurrences > 1)
-                    //{
-                    //    columnName = columnName + "_" + occurrences;
-                    //}
+                    int occurrences = columnNames.Count(x => x.Equals(columnName));
+                    if (occurrences > 1)
+                    {
+                        columnName = columnName + "_" + occurrences;
+                    }
 
                     //add the column to the datatable
                     dataTable[i].Columns.Add(columnName);
@@ -121,7 +114,7 @@ namespace ExcelReader
                 throw new ArgumentNullException("The DataTable was not found.");
                 //return;
             }
-            for (int i=0;i<dataTable.Length;i++)
+            for (int i = 0; i < dataTable.Length; i++)
             {
                 foreach (DataColumn col in dataTable[i].Columns)
                 {
